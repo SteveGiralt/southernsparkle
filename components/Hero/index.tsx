379227861e858bfa104eee2315bb011839b758fd
coreydismyname <corey.dubia@gmail.com>
2023-09-01 11:02:06 -0600

    Test change

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -15,17 +15,20 @@ const Hero = () => {
                 data-wow-delay=".2s"
               >
                 <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
-                Free and Open-Source Next.js Template for Startup & SaaS
+                  Free, Open-Source Next.js Template for Startup & SaaS
                 </h1>
                 <p className="mb-12 text-base font-medium !leading-relaxed text-body-color dark:text-white dark:opacity-90 sm:text-lg md:text-xl">
-                Startup is free Next.js template for startups and SaaS business websites comes with all the essential pages, components, and sections you need to launch a complete business website, built-with Next 13.x and Tailwind CSS.
+                  Startup is free Next.js template for startups and SaaS
+                  business websites comes with all the essential pages,
+                  components, and sections you need to launch a complete
+                  business website, built-with Next 13.x and Tailwind CSS.
                 </p>
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
                   <Link
                     href="https://nextjstemplates.com/templates/saas-starter-startup"
                     className="rounded-md bg-primary py-4 px-8 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80"
                   >
-                   🔥 Get Pro
+                    🔥 Get Pro
                   </Link>
                   <Link
                     href="https://github.com/NextJSTemplates/startup-nextjs"
