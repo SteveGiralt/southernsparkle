fdb5ea8840598ca53871f98c87a3b97339d71f7d
Steve Giralt <88471484+SteveGiralt@users.noreply.github.com>
2023-09-28 09:58:42 -0600

    swap out booking link

--- components/Services/ServiceDetail.tsx
+++ components/Services/ServiceDetail.tsx
@@ -21,7 +21,7 @@ const ServiceDetail = (props: {
   return (
     <section id="about" className="pt-16 md:pt-20 lg:pt-28">
       <div className="container">
-        <div className="border-b border-body-color/[.15] pb-16 dark:border-white/[.15] md:pb-20 lg:pb-28">
+        <div className="dark:border-white/[.15] border-b border-body-color/[.15] pb-16 md:pb-20 lg:pb-28">
           <div className="-mx-4 flex flex-wrap items-center">
             <div className="w-full px-4 lg:w-1/2">
               <SectionTitle title={title} paragraph={paragraph} mb="44px" />
@@ -43,7 +43,7 @@ const ServiceDetail = (props: {
             </div>
           </div>
           <div className="mt-2 flex items-center justify-center">
-            <Link href="/contact/">
+            <Link href="https://reneebrinson.setmore.com/">
               <button className="flex w-full items-center justify-center rounded-md bg-dark p-3 text-base font-semibold text-white transition duration-300 ease-in-out hover:bg-opacity-80 hover:shadow-signUp">
                 Book Now
               </button>
