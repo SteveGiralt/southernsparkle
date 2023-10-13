83cc835ab1d87b1ab9896a182534df80ed48b464
Steve Giralt <88471484+SteveGiralt@users.noreply.github.com>
2023-10-13 13:31:43 -0600

    swap out text

--- app/services/weight-loss/page.tsx
+++ app/services/weight-loss/page.tsx
@@ -5,7 +5,7 @@ const WeightLossDetail = (props) => {
     <ServiceDetail
       image="/images/services/weight-loss-detail.jpg"
       title="Tailored Prescription Solutions for Your Health Goals"
-      paragraph="We understand that achieving your weight goals can be a challenging endeavor, and that's why we offer personalized and effective prescription weight loss solutions. Led by the experienced and compassionate Renee, our team is dedicated to helping you embark on a transformative and sustainable weight loss journey. With a comprehensive understanding of the complexities of weight management, Renee provides tailored programs that consider your individual needs, preferences, and health history. Our approach integrates cutting-edge medical knowledge with a nurturing touch, ensuring that you receive the guidance, support, and resources necessary to reach your desired weight in a safe and healthy manner. At Southern Sparkle Aesthetics, we believe in empowering you to not just achieve your weight goals but to also embrace a vibrant, balanced, and fulfilling lifestyle."
+      paragraph="Welcome to Southern Sparkle Aesthetics, where your well-being is our priority. Renee is here to provide you with a personalized and effective Semaglutide weight loss treatment through weekly injections. With Renee's guidance, we ensure a comprehensive understanding of your unique health needs and weight management goals. Our dedicated focus on your journey towards sustainable weight loss is enhanced by the support and expertise of Renee, making your experience at Southern Sparkle Aesthetics both transformative and empowering. Trust Renee to help you achieve your desired results and embark on a healthier lifestyle."
     />
   );
 };
